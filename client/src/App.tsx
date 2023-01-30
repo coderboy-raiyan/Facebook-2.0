bec3f82bdbc3513016bc069079fdc3340a3b1a42
Raiyan <tajkierhaque@gmail.com>
2023-01-30 19:30:59 +0600

    Frontend code has been started [Add]

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+
 function App() {
-  return <div>App</div>;
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
---
+++ client/src/pages/profile/index.tsx
@@ -0,0 +1,5 @@
+function Profile() {
+    return <div>index</div>;
+}
+
+export default Profile;

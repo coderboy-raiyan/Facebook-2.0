15288aa7ed31713da35ac2772b1e164a90d0f39c
Raiyan <tajkierhaque@gmail.com>
2023-02-02 00:21:13 +0600

    Frontend authentication has been started [Started]

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,5 +1,32 @@
+import { Form, Formik } from "formik";
+import { Link } from "react-router-dom";
+
 function Login() {
-    return <div>index</div>;
+    return (
+        <section className="login">
+            <div>
+                {/* form header */}
+                <div>
+                    <img loading="lazy" src="../../icons/facebook.svg" alt="" />
+                    <h4>Facebook helps you to connect and share with the people in your life</h4>
+                </div>
+
+                {/* form */}
+                <div>
+                    <Formik>
+                        {(formik) => (
+                            <Form>
+                                <input type="email" />
+                                <input type="password" />
+                                <button type="submit">Login</button>
+                            </Form>
+                        )}
+                    </Formik>
+                    <Link to="/register">Forgotten Password ?</Link>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Login;
